7c0aafaea83e079756a66c9d9e0f5259aa3d3c50
LadyBetancourt <lady_betancourt24@unikino.edu.mx>
2025-10-21 22:09:49 -0700

    cambios

--- Arreglos/Cola.cs
+++ Arreglos/Cola.cs
@@ -2,5 +2,50 @@
 {
     internal class Cola
     {
+        int _inicio;
+        int _final;
+        string[] _arreglo;
+
+        public Cola(int elementos)
+        {
+            _inicio = 0;
+            _final = 0;
+            _arreglo = new string[elementos];
+        }
+
+        // Agrega un dato al final de la cola
+        public void Agregar(string dato)
+        {
+            int max = _arreglo.Length - 1;
+            if (_final > max)
+            {
+                throw new Exception("Cola llena");
+            }
+            _arreglo[_final] = dato;
+            _final++;
+        }
+
+        // Elimina el primer dato en entrar
+        public void Eliminar()
+        {
+            if (_final == 0 || _inicio == _final)
+            {
+                throw new Exception("Cola vacía");
+            }
+
+            _arreglo[_inicio] = string.Empty;
+            _inicio++;
+        }
+
+        // Aqui muestra el contenido de la cola
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+            for (int i = _inicio; i < _final; i++)
+            {
+                datos += _arreglo[i] + Environment.NewLine;
+            }
+            return datos;
+        }
     }
 }
